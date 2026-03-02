c1ce2fd7209c5af59913ca8700465d8f1c4e3ca7
jdpreston30 <jdpreston30@gmail.com>
2026-03-02 15:18:49 -0500

    updates

--- All_Run/run.R
+++ All_Run/run.R
@@ -4,6 +4,7 @@ config <- load_dynamic_config(computer = "auto", config_path = "All_Run/config_d
 source("R/Scripts/00a_environment_setup.R")
 source("R/Scripts/00b_setup.R")
 source("R/Scripts/00c_import.R")
+source("R/Scripts/00d_cleanup.R")
 source("R/Scripts/01_descriptive.R")
 source("R/Scripts/02_mgmt_stratify.R")
 source("R/Scripts/03_grade_stratify.R")
